3ff23b7d21c3a147264c59928f1a49733edaa7f1
Paulogui89 <paulo_phs89@hotmail.com>
2022-04-21 16:31:39 -0300

    crud sem db

--- src/main/java/com/paulohenrique/springbasicapi/controller/LibraryController.java
+++ src/main/java/com/paulohenrique/springbasicapi/controller/LibraryController.java
@@ -2,9 +2,7 @@ package com.paulohenrique.springbasicapi.controller;
 
 import com.paulohenrique.springbasicapi.model.Library;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +17,41 @@ public class LibraryController {
     @GetMapping("/list")
     public ResponseEntity<List<Library>> returnList(){
         return ResponseEntity.ok(Library.list);
+    }
+
+    @PostMapping("/library")
+    public ResponseEntity<Library> create(@RequestBody Library lib){
+        return ResponseEntity.ok(lib);
+    }
+
+    @GetMapping("/library/{id}")
+    public ResponseEntity<Library> read(@PathVariable long id) throws Exception {
+        Library lib = Library.list.stream().filter(l -> l.getId() == id)
+                .findFirst().orElseThrow(() ->new Exception("invalid id: " + id));
+
+        return ResponseEntity.ok(lib);
+    }
+
+    @PutMapping("/library/{id}")
+    public ResponseEntity<Library> update(@RequestBody Library lib, @PathVariable long id) throws Exception {
+        Library.list.remove(Library.list.size()-1);
+        Library updateLib = Library.list.stream().filter(l -> l.getId() == id)
+                .findFirst().orElseThrow(() ->new Exception("invalid id: " + id));
+        updateLib.setAuthorName(lib.getAuthorName());
+        updateLib.setBookName(lib.getBookName());
+        updateLib.setPublishedDate(lib.getPublishedDate());
+
+        return ResponseEntity.ok(updateLib);
+    }
+
+    @DeleteMapping("/library/{id}")
+    public ResponseEntity<Library> delete(@PathVariable long id) throws Exception {
+        Library lib = Library.list.stream().filter(l -> l.getId() == id)
+                .findFirst().orElseThrow(() ->new Exception("invalid id: " + id));
+
+        Library.list.remove(lib);
+
+        return ResponseEntity.ok(lib);
     }
 
 }
